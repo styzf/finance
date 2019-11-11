a01903aee9572a606d5a4eb701628d67b56c004c
styzf <styzfa@163.com>
2019-11-11 15:05:18 +0800

    新增用户相关提交

---
+++ sso/sso-api/src/main/java/com/styzf/sso/dto/response/ResultCode.java
@@ -0,0 +1,15 @@
+package com.styzf.sso.dto.response;
+
+/**
+ * @author yangzf
+ * @date 2019-09-23
+ */
+public interface ResultCode {
+    //操作是否成功,true为成功，false操作失败
+    boolean success();
+    //操作代码
+    int code();
+    //提示信息
+    String message();
+
+}
