08d5b9110c5e7e0ac505bc996fbcc93bb898a526
styzf <styzfa@163.com>
2019-11-12 14:48:20 +0800

    添加redis配置，命名修改

---
+++ sso/sso-api/src/main/java/com/styzf/finance/dto/user/MenuDTO.java
@@ -0,0 +1,36 @@
+package com.styzf.finance.dto.user;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+/**
+ * @author styzf
+ * @date 2019-11-11
+ */
+@Data
+@ToString
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="权限表", description="权限表")
+public class MenuDTO {
+    
+    private String id;
+    private String code;
+    private String pCode;
+    private String pId;
+    private String menuName;
+    private String url;
+    private String isMenu;
+    private Integer level;
+    private Integer sort;
+    private String status;
+    private String icon;
+    private Date createTime;
+    private Date updateTime;
+
+}
---
+++ sso/sso-service/src/main/java/com/styzf/finance/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,85 @@
+package com.styzf.finance.service.impl;
+
+import com.styzf.finance.dto.user.MenuDTO;
+import com.styzf.finance.dto.user.UserExt;
+import com.styzf.finance.service.UserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO 后续需要修改
+ * TODO 1、用户信息的查询要查数据库，非写死
+ * TODO 2、因为依赖可能不依赖UserDetailsService，不进行注册服务，考虑使用其他的接口调用，或者不对外提供
+ * @author styzf
+ * @date 2019-09-23
+ */
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private UserService userService;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //取出身份，如果身份为空说明没有认证
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //没有认证统一采用httpbasic认证，httpbasic中存储了client_id和client_secret，开始认证client_id和client_secret
+        if(authentication==null){
+            ClientDetails clientDetails = clientDetailsService.loadClientByClientId(username);
+            if(clientDetails!=null){
+                //密码
+                String clientSecret = clientDetails.getClientSecret();
+                return new User(username,clientSecret,AuthorityUtils.commaSeparatedStringToAuthorityList(""));
+            }
+        }
+        if (StringUtils.isEmpty(username)) {
+            return null;
+        }
+        //远程调用用户中心根据账号查询用户信息
+        UserExt userext = userService.getUserExt(username);
+        if(userext == null){
+            //返回空给spring security表示用户不存在
+            return null;
+        }
+    
+        //取出正确密码（hash值）
+        String password = userext.getPassword();
+        //用户权限，这里暂时使用静态数据，最终会从数据库读取
+        //从数据库获取权限
+        List<MenuDTO> permissions = userext.getPermissions();
+        if(permissions == null){
+            permissions = new ArrayList<>();
+        }
+        List<String> user_permission = new ArrayList<>();
+        permissions.forEach(item-> user_permission.add(item.getCode()));
+        //使用静态的权限表示用户所拥有的权限
+        String user_permission_string  = StringUtils.join(user_permission.toArray(), ",");
+        UserJwt userDetails = new UserJwt(username,
+                password,
+                AuthorityUtils.commaSeparatedStringToAuthorityList(user_permission_string));
+        userDetails.setId(userext.getId());
+        userDetails.setUtype(userext.getUtype());//用户类型
+        userDetails.setCompanyId(userext.getCompanyId());//所属企业
+        userDetails.setName(userext.getName());//用户名称
+        userDetails.setUserpic(userext.getUserpic());//用户头像
+        return userDetails;
+    }
+}
