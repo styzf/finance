2647101d251c2997a2d925998ccd858bc0dcae3a
styzf <styzfa@163.com>
2019-12-13 10:45:57 +0800

    war包部署

--- eureka-center/src/main/java/com/styzf/govern/center/GovernCenterApplication.java
+++ eureka-center/src/main/java/com/styzf/govern/center/GovernCenterApplication.java
@@ -2,6 +2,8 @@ package com.styzf.govern.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -10,8 +12,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  **/
 @EnableEurekaServer //标识此工程是一个EurekaServer
 @SpringBootApplication
-public class GovernCenterApplication {
+public class GovernCenterApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(GovernCenterApplication.class,args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GovernCenterApplication.class);
     }
 }
