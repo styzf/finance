64089ef8176ebad36e6b4c305218055f1f145b78
styzf <styzfa@163.com>
2019-12-20 13:49:59 +0800

    Swagger2Config添加请求头参数

--- core/web/src/main/java/com/styzf/core/web/config/swagger2/Swagger2Config.java
+++ core/web/src/main/java/com/styzf/core/web/config/swagger2/Swagger2Config.java
@@ -6,12 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.annotations.ApiIgnore;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -19,11 +25,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger2Config {
     @Bean
     public Docket springfoxDocket(ApiInfoProperties apiInfo) {
+	
+	    ParameterBuilder authPar = new ParameterBuilder();
+	    List<Parameter> pars = new ArrayList<Parameter>();
+	    authPar.name("Authorization").description("jwt令牌")
+			    .modelRef(new ModelRef("string")).parameterType("header")
+			    .required(false).build(); //header中的ticket参数非必填，传空也可以
+	    pars.add(authPar.build());    //根据每个方法名也知道当前方法在设置什么参数
+	    
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(new Class[] { ApiIgnore.class })
                 .apiInfo(apiInfo(apiInfo))
                 .pathMapping("/").select().paths(PathSelectors.regex("^.*(?<!error)$")).build()
-                .select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+                .select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build()
+		        .globalOperationParameters(pars);
         return docket;
     }
     
