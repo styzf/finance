190ed0db76199d1a71b1db1060a8aa5ce38d0a72
styzf <styzfa@163.com>
2019-12-25 14:18:23 +0800

    添加个注解试一下

--- finance/finance-service/src/main/java/com/styzf/finance/filter/AuthSettings.java
+++ finance/finance-service/src/main/java/com/styzf/finance/filter/AuthSettings.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
@@ -17,9 +18,10 @@ import java.util.Map;
  * @date 2019-12-25
  */
 @Data
-@Component("authSettings")
+@Component
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@Configuration
 @ConfigurationProperties(prefix="styzf.auth")
 public class AuthSettings implements Serializable {
     
