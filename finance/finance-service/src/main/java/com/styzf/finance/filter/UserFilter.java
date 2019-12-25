a8af5c51dfb492d5525a968b7516f12b50f89139
styzf <styzfa@163.com>
2019-12-25 10:33:06 +0800

    先做简单的处理

--- finance/finance-service/src/main/java/com/styzf/finance/filter/UserFilter.java
+++ finance/finance-service/src/main/java/com/styzf/finance/filter/UserFilter.java
@@ -31,27 +31,30 @@ public class UserFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		log.error(request.getClass().getName());
 		UserHttpServletRequestWrapper req = new UserHttpServletRequestWrapper((HttpServletRequest) request);
-		Cookie[] cookies = req.getCookies();
-		String token = "";
-		if (Objects.nonNull(cookies)) {
-			for (Cookie cookie : cookies) {
-				if ("uid".equals(cookie.getName())) {
-					token = cookie.getValue();
-					break;
+		try {
+			chain.doFilter(req, response);
+		} catch (Exception e) {
+			Cookie[] cookies = req.getCookies();
+			String token = "";
+			if (Objects.nonNull(cookies)) {
+				for (Cookie cookie : cookies) {
+					if ("uid".equals(cookie.getName())) {
+						token = cookie.getValue();
+						break;
+					}
 				}
 			}
-		}
-		if (StringUtils.isNotBlank(token)) {
-			AuthToken authToken = redisUtil.getObject("user:auth:user_token:" + token, AuthToken.class);
-			if (Objects.nonNull(authToken)) {
-				String jwt = authToken.getJwt_token();
-				req.putHeader("Authorization", "Bearer " + jwt);
+			if (StringUtils.isNotBlank(token)) {
+				AuthToken authToken = redisUtil.getObject("user:auth:user_token:" + token, AuthToken.class);
+				if (Objects.nonNull(authToken)) {
+					String jwt = authToken.getJwt_token();
+					req.putHeader("Authorization", "Bearer " + jwt);
+				}
 			}
+			
+			chain.doFilter(req, response);
 		}
-
-		chain.doFilter(req, response);
 	}
 
 	@Override
