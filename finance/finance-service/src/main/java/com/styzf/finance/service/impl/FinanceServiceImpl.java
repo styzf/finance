7a46a3b0e26a1064800f894c38d377c1c895022a
styzf <styzfa@163.com>
2020-05-20 16:04:41 +0800

    账本数据不需要去重处理

--- finance/finance-service/src/main/java/com/styzf/finance/service/impl/FinanceServiceImpl.java
+++ finance/finance-service/src/main/java/com/styzf/finance/service/impl/FinanceServiceImpl.java
@@ -2,6 +2,8 @@ package com.styzf.finance.service.impl;
 
 import com.styzf.core.common.util.ConvertUtil;
 import com.styzf.core.common.util.date.DateUtil;
+import com.styzf.finance.constant.CategoryConstant;
+import com.styzf.finance.dto.category.CategoryDTO;
 import com.styzf.finance.dto.category.CategoryTree;
 import com.styzf.finance.dto.finance.FinanceDTO;
 import com.styzf.finance.dto.finance.FinanceListData;
@@ -30,108 +32,120 @@ import java.util.*;
 @Component
 @Transactional(rollbackFor = RuntimeException.class)
 public class FinanceServiceImpl extends BaseServiceImpl<Finance, FinanceDTO> implements IFinanceService {
-	
-	@Autowired
-	private ICategoryService categoryService;
-	
-	@Override
-	public void addRemark(FinanceRemarkDTO dto) {
-		String remark = dto.getRemark();
-		FinanceDTO addDTO = ConvertUtil.convert(dto, FinanceDTO.class);
-		addDTO.setRemark(null);
-		List<FinanceDTO> list = baseList(addDTO);
-		if (CollectionUtils.isEmpty(list)) {
-			addDTO.setId(null);
-			addDTO.setRemark(remark);
-		} else {
-			addDTO = list.get(0);
-			addDTO.setRemark(remark);
-		}
-		baseInsertOrUpdate(addDTO);
-	}
-	
-	@Override
-	public List<FinanceListData> getFinance(Integer year, Integer month) {
-		Calendar cal = Calendar.getInstance();
-		if (Objects.isNull(year)) {
-			year = cal.get(Calendar.YEAR);
-		}
-		if (Objects.isNull(month)) {
-			month = cal.get(Calendar.MONTH )+1;
-		}
-		
-		CategoryTree categoryTree = categoryService.getCategoryTree(1L);
-		List<CategoryTree> list = categoryTree.getChildList();
-		
-		List<FinanceListData> dataList = new ArrayList<>();
-		setData(list, dataList, year, month, false);
-		return dataList;
-	}
-	
-	/**
-	 * 设置财务数据
-	 * @param list 分类树列表
-	 * @param dataList 保存数据的列表
-	 * @param year 年
-	 * @param month 月
-	 * @param isChild 是否是子节点
-	 */
-	private void setData(List<CategoryTree> list, List<FinanceListData> dataList,
-	                     Integer year, Integer month, boolean isChild) {
-		if (CollectionUtils.isEmpty(list)) {
-			return;
-		}
-		for (CategoryTree tree: list) {
-			FinanceListData data = ConvertUtil.convert(tree, FinanceListData.class);
-			List<FinanceDTO> dtoList = getFinanceList(data.getId(), year, month);
-			data.setData(dtoList).
-			     setChild(isChild);
-			dataList.add(data);
-			List<CategoryTree> childList = tree.getChildList();
-			setData(childList, dataList, year, month, true);
-		}
-	}
-	
-	/**
-	 * 获取财务数据
-	 * @param categoryId 分类id
-	 * @param year 年
-	 * @param month 月
-	 * @return 财务数据列表
-	 */
-	private List<FinanceDTO> getFinanceList(Long categoryId, Integer year, Integer month) {
-		FinanceDTO dto = new FinanceDTO();
-		dto.setCategoryId(categoryId).
-		    setYear(year).
-		    setMonth(month).
-			setDeleteFlag(DeleteEnum.EXIST.getStatus());
-		return baseList(dto);
-	}
-	
-	@Override
-	protected void insertOrUpdateBefore(FinanceDTO dto) {
-		if (Objects.isNull(dto.getCreateTime()) &&
-			Objects.nonNull(dto.getYear()) &&
-			Objects.nonNull(dto.getMonth()) &&
-			Objects.nonNull(dto.getDay())) {
-			Date date = DateUtil.stringToDate("" + dto.getYear() + "-" + dto.getMonth() + "-" + dto.getDay());
-			dto.setCreateTime(date);
-		}
-		
-		if (Objects.nonNull(dto.getId())) {
-			return;
-		}
-		Weekend<Finance> weekend = Weekend.of(Finance.class);
-		weekend.weekendCriteria().
-				andEqualTo(Finance::getYear, dto.getYear()).
-				andEqualTo(Finance::getMonth, dto.getMonth()).
-				andEqualTo(Finance::getDay, dto.getDay()).
-				andEqualTo(Finance::getCategoryId, dto.getCategoryId()).
-				andEqualTo(Finance::getDeleteFlag, Boolean.FALSE);
-		List<Finance> financeList = mapper.selectByExample(weekend);
-		if (! CollectionUtils.isEmpty(financeList)) {
-			Long id = financeList.get(0).getId();
-			dto.setId(id);
-		}
-	}
+    
+    @Autowired
+    private ICategoryService categoryService;
+    
+    @Override
+    public void addRemark(FinanceRemarkDTO dto) {
+        String remark = dto.getRemark();
+        FinanceDTO addDTO = ConvertUtil.convert(dto, FinanceDTO.class);
+        addDTO.setRemark(null);
+        List<FinanceDTO> list = baseList(addDTO);
+        if (CollectionUtils.isEmpty(list)) {
+            addDTO.setId(null);
+            addDTO.setRemark(remark);
+        } else {
+            addDTO = list.get(0);
+            addDTO.setRemark(remark);
+        }
+        baseInsertOrUpdate(addDTO);
+    }
+    
+    @Override
+    public List<FinanceListData> getFinance(Integer year, Integer month) {
+        Calendar cal = Calendar.getInstance();
+        if (Objects.isNull(year)) {
+            year = cal.get(Calendar.YEAR);
+        }
+        if (Objects.isNull(month)) {
+            month = cal.get(Calendar.MONTH) + 1;
+        }
+        
+        CategoryTree categoryTree = categoryService.getCategoryTree(1L);
+        List<CategoryTree> list = categoryTree.getChildList();
+        
+        List<FinanceListData> dataList = new ArrayList<>();
+        setData(list, dataList, year, month, false);
+        return dataList;
+    }
+    
+    /**
+     * 设置财务数据
+     *
+     * @param list     分类树列表
+     * @param dataList 保存数据的列表
+     * @param year     年
+     * @param month    月
+     * @param isChild  是否是子节点
+     */
+    private void setData(List<CategoryTree> list, List<FinanceListData> dataList,
+                         Integer year, Integer month, boolean isChild) {
+        if (CollectionUtils.isEmpty(list)) {
+            return;
+        }
+        for (CategoryTree tree : list) {
+            FinanceListData data = ConvertUtil.convert(tree, FinanceListData.class);
+            List<FinanceDTO> dtoList = getFinanceList(data.getId(), year, month);
+            data.setData(dtoList).
+                    setChild(isChild);
+            dataList.add(data);
+            List<CategoryTree> childList = tree.getChildList();
+            setData(childList, dataList, year, month, true);
+        }
+    }
+    
+    /**
+     * 获取财务数据
+     *
+     * @param categoryId 分类id
+     * @param year       年
+     * @param month      月
+     * @return 财务数据列表
+     */
+    private List<FinanceDTO> getFinanceList(Long categoryId, Integer year, Integer month) {
+        FinanceDTO dto = new FinanceDTO();
+        dto.setCategoryId(categoryId).
+                setYear(year).
+                setMonth(month).
+                setDeleteFlag(DeleteEnum.EXIST.getStatus());
+        return baseList(dto);
+    }
+    
+    @Override
+    protected void insertOrUpdateBefore(FinanceDTO dto) {
+        if (Objects.isNull(dto.getCreateTime()) &&
+                Objects.nonNull(dto.getYear()) &&
+                Objects.nonNull(dto.getMonth()) &&
+                Objects.nonNull(dto.getDay())) {
+            Date date = DateUtil.stringToDate("" + dto.getYear() + "-" + dto.getMonth() + "-" + dto.getDay());
+            dto.setCreateTime(date);
+        }
+        
+        if (Objects.nonNull(dto.getId())) {
+            return;
+        }
+        
+        Long categoryId = dto.getCategoryId();
+        CategoryDTO category = categoryService.baseGetById(categoryId);
+    
+        if (Objects.isNull(category)) {
+            return;
+        }
+        
+        if (! CategoryConstant.BOOK_CATEGORY_KEY.equals(category.getParentKey())) {
+            Weekend<Finance> weekend = Weekend.of(Finance.class);
+            weekend.weekendCriteria().
+                    andEqualTo(Finance::getYear, dto.getYear()).
+                    andEqualTo(Finance::getMonth, dto.getMonth()).
+                    andEqualTo(Finance::getDay, dto.getDay()).
+                    andEqualTo(Finance::getCategoryId, dto.getCategoryId()).
+                    andEqualTo(Finance::getDeleteFlag, Boolean.FALSE);
+            List<Finance> financeList = mapper.selectByExample(weekend);
+            if (!CollectionUtils.isEmpty(financeList)) {
+                Long id = financeList.get(0).getId();
+                dto.setId(id);
+            }
+        }
+    }
 }
