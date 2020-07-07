1fdc557b1a82ebbe68fe3f5ea62309110d4f2278
styzf <styzfa@163.com>
2020-07-07 14:43:43 +0800

    权限接口提交

---
+++ sso/sso-service/src/main/java/com/styzf/sso/service/MenuService.java
@@ -0,0 +1,13 @@
+package com.styzf.sso.service;
+
+import com.styzf.core.common.base.BaseService;
+import com.styzf.sso.dto.user.MenuDTO;
+
+/**
+ * 权限接口
+ * @author styzf
+ * @date 2020-07-07
+ */
+public interface MenuService extends BaseService<MenuDTO> {
+
+}
---
+++ sso/sso-service/src/main/java/com/styzf/sso/service/impl/MenuServiceImpl.java
@@ -0,0 +1,16 @@
+package com.styzf.sso.service.impl;
+
+import com.styzf.mybatis.base.BaseServiceImpl;
+import com.styzf.sso.dto.user.MenuDTO;
+import com.styzf.sso.po.Menu;
+import com.styzf.sso.service.MenuService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author styzf
+ * @date 2020-07-07
+ **/
+@Service
+public class MenuServiceImpl extends BaseServiceImpl<Menu, MenuDTO> implements MenuService {
+
+}
