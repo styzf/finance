9b0f83cb0f4aa30fa06f342b0f1684df7ba3d148
styzf <styzfa@163.com>
2020-07-07 14:24:54 +0800

    权限文档提交

---
+++ sso/sso-service/src/main/java/com/styzf/sso/web/doc/MenuControllerDoc.java
@@ -0,0 +1,14 @@
+package com.styzf.sso.web.doc;
+
+import com.styzf.core.web.base.BaseControllerDoc;
+import com.styzf.sso.dto.user.MenuDTO;
+import io.swagger.annotations.Api;
+
+/**
+ * @author styzf
+ * @date 2020-07-07
+ */
+@Api(value = "权限信息",description = "权限信息接口")
+public interface MenuControllerDoc extends BaseControllerDoc<MenuDTO> {
+
+}
