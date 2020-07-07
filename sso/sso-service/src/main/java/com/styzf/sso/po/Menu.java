928ccaba740e869e2c5d5f5380b1ad9fc890bcf8
styzf <styzfa@163.com>
2020-07-07 10:12:30 +0800

    注释一下

--- sso/sso-service/src/main/java/com/styzf/sso/po/Menu.java
+++ sso/sso-service/src/main/java/com/styzf/sso/po/Menu.java
@@ -26,19 +26,58 @@ import java.util.Date;
 @ApiModel(value="权限表", description="权限表")
 public class Menu extends BasePO {
     
+    /**
+     * 菜单编码
+     */
     private String code;
+    
+    /**
+     * 父菜单编码
+     */
     @Column(name="p_code")
     private String pCode;
+    
+    /**
+     * 父菜单id
+     */
     @Column(name="p_id")
     private Long pId;
+    
+    /**
+     * 菜单名
+     */
     @Column(name="menu_name")
     private String menuName;
+    
+    /**
+     * 请求地址
+     */
     private String url;
+    
+    /**
+     * 是否是菜单
+     */
     @Column(name="is_menu")
     private String isMenu;
+    
+    /**
+     * 菜单层级
+     */
     private Integer level;
+    
+    /**
+     * 菜单排序
+     */
     private Integer sort;
+    
+    /**
+     * 状态
+     */
     private String status;
+    
+    /**
+     * 图标样式，这个应该没啥用
+     */
     private String icon;
 
 }
