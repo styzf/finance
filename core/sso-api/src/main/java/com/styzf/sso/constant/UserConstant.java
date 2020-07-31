4ea2f89b039f6eb09f7d41ef7e3e19f6b3e3319e
styzf <styzfa@163.com>
2020-07-31 11:36:12 +0800

    用户常量提交

---
+++ core/sso-api/src/main/java/com/styzf/sso/constant/UserConstant.java
@@ -0,0 +1,19 @@
+package com.styzf.sso.constant;
+
+/**
+ * 用户常量
+ */
+public interface UserConstant {
+	
+	interface Menu {
+		/**
+		 * 所有权限最高的分类级别
+		 */
+		Long ROOT_ID = 0L;
+
+		/**
+		 * 所有权限最高的分类级别
+		 */
+		String ROOT_KEY = "root";
+	}
+}
