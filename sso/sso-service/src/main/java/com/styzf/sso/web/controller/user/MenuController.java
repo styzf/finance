34fa0f5b1d424af8c64ac61f851dc70c07782034
styzf <styzfa@163.com>
2020-07-31 13:53:36 +0800

    权限树及权限树接口文档提供

--- sso/sso-service/src/main/java/com/styzf/sso/web/controller/user/MenuController.java
+++ sso/sso-service/src/main/java/com/styzf/sso/web/controller/user/MenuController.java
@@ -1,8 +1,13 @@
 package com.styzf.sso.web.controller.user;
 
+import com.styzf.core.common.response.Response;
+import com.styzf.core.common.response.SuccessResponseData;
 import com.styzf.core.web.base.BaseController;
 import com.styzf.sso.dto.user.MenuDTO;
+import com.styzf.sso.dto.user.MenuTree;
+import com.styzf.sso.service.MenuService;
 import com.styzf.sso.web.doc.MenuControllerDoc;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user/menu")
 public class MenuController extends BaseController<MenuDTO> implements MenuControllerDoc {
-
+    
+    @Override
+    @GetMapping("/tree")
+    public Response getMenuTree(Long parentId) {
+        MenuTree menuTree = ((MenuService) service).getMenuTree(parentId);
+        return SuccessResponseData.newInstance(menuTree);
+    }
 }
--- sso/sso-service/src/main/java/com/styzf/sso/web/doc/MenuControllerDoc.java
+++ sso/sso-service/src/main/java/com/styzf/sso/web/doc/MenuControllerDoc.java
@@ -1,8 +1,10 @@
 package com.styzf.sso.web.doc;
 
+import com.styzf.core.common.response.Response;
 import com.styzf.core.web.base.BaseControllerDoc;
 import com.styzf.sso.dto.user.MenuDTO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author styzf
@@ -10,5 +12,12 @@ import io.swagger.annotations.Api;
  */
 @Api(value = "权限信息",description = "权限信息接口")
 public interface MenuControllerDoc extends BaseControllerDoc<MenuDTO> {
-
+    
+    /**
+     * 根据父分类id获取权限树列表
+     * @param parentId 父id
+     * @return 权限树
+     */
+    @ApiOperation("根据父分类id获取权限树列表")
+    Response getMenuTree(Long parentId);
 }
